01dd06d8767bb2f1b0aa757697374f409d384872
dtaylorbrown <daniella.taylor92@gmail.com>
2025-11-07 15:51:57 +0000

    :rocket: astro and content setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
-export default defineConfig({});
+import netlify from '@astrojs/netlify';
+
+export default defineConfig({
+  //   site: 'https://example.com',
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  adapter: netlify(),
+});
